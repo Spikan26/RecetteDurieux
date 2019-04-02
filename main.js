335e674d1d0fb2da71af2d7725d894f2d0be5562
Alexandre <32333127+Spikan26@users.noreply.github.com>
2019-04-02 10:03:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -36,10 +36,10 @@ function Table () {
     textdata.forEach(function (d) {
         if(newinput == " "){
             console.log(d.name);
-            $(".bodytab").append("<tr><th scope='row'>" + d.name +"</th><td><img src='" + d.image + "' /></td></tr>");
+            $(".bodytab").append("<tr><th scope='row' style='vertical-align: middle;'>" + d.name +"</th><td><img src='" + d.image + "' height="50px" width="50px" /></td></tr>");
         }
         else if((d.name).startsWith(newinput)){
-            $(".bodytab").append("<tr><th scope='row'>" + d.name +"</th><td><img src='" + d.image + "' /></td></tr>");
+            $(".bodytab").append("<tr><th scope='row' style='vertical-align: middle;'>" + d.name +"</th><td><img src='" + d.image + "' height="50px" width="50px" /></td></tr>");
         }
     })
 }
